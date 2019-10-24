591d357e95aeac3b4fda7b02719bcc53ea70a1d7
Jeremy Vun <2726092@student.swin.edu.au>
2019-10-25 04:06:19 +1100

    fix registration email field

--- drop/serializers.py
+++ drop/serializers.py
@@ -6,16 +6,15 @@ class UserSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = User
 		fields = ('id', 'username', 'password', 'email')
-		write_only_fields=('password',)
-		read_only_fields=('id',)
+		write_only_fields = ('password',)
+		read_only_fields = ('id',)
 
 	def create(self, validated_data):
 		user = User.objects.create(
-			username = validated_data['username'],
-			email = validated_data['username']
+			username=validated_data['username'],
+			email=validated_data['email']
 		)
 
 		user.set_password(validated_data['password'])
 		user.save()
 		return user
-
