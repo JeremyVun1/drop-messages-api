ff3d85c856c6876d52c6df97328efa93f02623d2
Jeremy Vun <2726092@student.swin.edu.au>
2019-10-27 19:46:12 +1100

    fixes

--- drop/constants.py
+++ drop/constants.py
@@ -3,6 +3,6 @@
 
 DELETE_THRESH = -5  # vote threshhold for message deletion
 GEOLOC_RESOLUTION = 2  # decimal places of geolocation precision
-MAX_MESSAGE_LENGTH = 140 # maximum length of a message
+MAX_MESSAGE_LENGTH = 240 # maximum length of a message
 MAX_RANGE = 2 # maximum search radius for messages around a geoloc
 PAGE_SIZE = 10
